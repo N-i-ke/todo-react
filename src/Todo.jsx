7bb67f35afb6022735c1154711389bdc450277c4
N-i-ke <nike.forgithub@gmail.com>
2024-04-29 02:01:12 +0900

    削除機能
    indexを要素ごとに割り振る
    spliceで削除する

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -18,6 +18,11 @@ export const Todo = () => {
     setIncompleteTodos(newTodos);
     setTodoText("")
   }
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos)
+  }
   return (
     <>
       <div className="input-area">
@@ -27,13 +32,13 @@ export const Todo = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo,index) => {
             return (
               <li key={todo}>
                 <div className="list-row">
                   <p className="todo-item">{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDelete(index)}>削除</button>
                 </div>
               </li>
             );
